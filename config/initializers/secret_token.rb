cdc8e1b205ffac0dc6eb5ad8f021449c76992518
Christopher Thornton <rmdirbin@gmail.com>
2013-04-29 20:04:30 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dbmgmt::Application.config.secret_token = '0706ec97cc5608d7396594be11f432a3ab33765405e8f375ecc78860b66a53086e66acf0c887e39e190cff71f9648ebbc5090886e9b3ee1d0bc490417e72dadc'
---
+++ config/routes.rb
@@ -0,0 +1,4 @@
+Dbmgmt::Application.routes.draw do
+
+  root :to => 'welcome#index'
+end
