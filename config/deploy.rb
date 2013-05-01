b982c118106977e7c7d1cb3dc3d77ff16c42a067
Christopher Thornton <rmdirbin@gmail.com>
2013-05-01 15:31:59 -0700

    Fixed database and deploy items

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,14 @@
-set :application, "/home/christopher/web/cse412.cgthornt.com"
-set :repository,  "https://github.com/cgthornt/DB-Management-Honors-Contract.git"
+require 'bundler/capistrano'
+require "rvm/capistrano"
+
+
+set :applicationdir, "/home/christopher/web/cse412.cgthornt.com"
+set :application, "CSE412"
+set :repository,  "git://github.com/cgthornt/DB-Management-Honors-Contract.git"
+
+set :deploy_to, applicationdir
+set :deploy_via, :copy
+set :copy_strategy, :export
 
 set :scm, :git
 
@@ -7,11 +16,18 @@ role :web, "cse412.cgthornt.com"                          # Your HTTP server, Ap
 role :app, "cse412.cgthornt.com"                          # This may be the same as your `Web` server
 role :db,  "cse412.cgthornt.com", :primary => true # This is where Rails migrations will run
 
+set :rvm_ruby_string, '1.9.3'
+set :rvm_type, :system
+
+
 # if you want to clean up old releases on each deploy uncomment this:
 after "deploy:restart", "deploy:cleanup"
+after "deploy:update_code", "deploy:migrate"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
+
+default_run_options[:pty] = true
 
 # If you are using Passenger mod_rails uncomment this:
 namespace :deploy do
